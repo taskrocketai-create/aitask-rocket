d09a033bf890cf3d405523d65fc1f0cd212be7b4
Wix Remote Machine <support@wix.com>
2026-05-13 22:01:05 +0000

    Investigate site loading issue

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,10 +1,14 @@
 export const Head = () => {
   return (
     <>
+      <title>TaskRocket - Business Systems, Done Right</title>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="TaskRocket automates your business systems so you can focus on growth. We build custom workflows for service-based businesses." />
+      <meta name="theme-color" content="#617ac2" />
       {/* Fonts */}
       <link rel="preconnect" href="https://static.parastorage.com" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
     </>
   );
 };
